d128964bad53b36d16510749672b3427ab0e4508
EdoardaDepine <edoarda.depine@gmail.com>
2023-04-26 15:24:04 -0300

    implementando função de verificação de idade. A mesma retorna uma foto correspondente a idade informada pelo usuário

---
+++ script.js
@@ -0,0 +1,48 @@
+function checkAge() {
+  const date = new Date();
+  const actualYear = date.getFullYear();
+  const valueYearBirth = document.querySelector("#yearBirth").value;
+  const userAge = actualYear - Number(valueYearBirth);
+  const GenderCkeck = document.getElementsByName("radioGender");
+  const result = document.querySelector("#result");
+  const image = document.createElement("img");
+  image.setAttribute("id", "photo");
+
+  verify();
+
+  function verify() {
+    let gender = "";
+    if (GenderCkeck[0].checked) {
+      let gender = "Mulher";
+      if (userAge >= 0 && userAge <= 10) {
+        image.setAttribute("src", "./images/menina-bebe.jpg");
+      } else if (userAge < 30) {
+        image.setAttribute("src", "./images/mulher-jovem.jpg");
+      } else if (userAge < 50) {
+        image.setAttribute("src", "./images/mulher-adulta.jpg");
+      } else {
+        image.setAttribute("src", "./images/mulher-idosa.jpg");
+      }
+    } else if (GenderCkeck[1].checked) {
+      let gender = "Homem";
+      if (userAge >= 0 && userAge <= 10) {
+        image.setAttribute("src", "./images/menino-bebe.jpg");
+      } else if (userAge < 30) {
+        image.setAttribute("src", "./images/homem-jovem.jpg");
+      } else if (userAge < 50) {
+        image.setAttribute("src", "./images/homem-adulto.jpg");
+      } else {
+        image.setAttribute("src", "./images/homem-idoso.jpg");
+      }
+    }
+    result.innerHTML = `Detectamos gênero ${gender} com ${userAge} anos!`;
+    result.appendChild(image);
+  }
+}
+
+const buttonVerification = document.querySelector("#verifyButton");
+
+buttonVerification.addEventListener("click", (event) => {
+  event.preventDefault();
+  checkAge();
+});
